e53396ed0be6ae50f5205d8e8289ca376bb27057
krkan <68450597+krkan-xxx@users.noreply.github.com>
2021-01-17 19:29:23 +0100

    Add files via upload

---
+++ krkan_loading/__resource.lua
@@ -0,0 +1,12 @@
+files{
+    "loading/index.html",
+    "loading/app.js",
+    "loading/image1.jpg",
+    "loading/particles.js",
+    "loading/style.css",
+    "loading/music/music.ogg",
+}
+
+loadscreen 'loading/index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
